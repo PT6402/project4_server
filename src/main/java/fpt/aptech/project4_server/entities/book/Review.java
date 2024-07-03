e11a510a9c72051f71164665a0d2873676a312aa
unknown <uslb98@gmail.com>
2024-07-03 14:28:53 +0700

    Add review CRUD

--- src/main/java/fpt/aptech/project4_server/entities/book/Review.java
+++ src/main/java/fpt/aptech/project4_server/entities/book/Review.java
@@ -1,16 +1,21 @@
 package fpt.aptech.project4_server.entities.book;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import fpt.aptech.project4_server.entities.BaseEntity;
 import fpt.aptech.project4_server.entities.user.UserDetail;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Data
 @NoArgsConstructor
@@ -20,16 +25,21 @@ import lombok.experimental.SuperBuilder;
 @Entity
 @Table(name = "tbReview")
 public class Review extends BaseEntity {
-    
+
     private String content;
-    private int rating;
+    private double rating;
+
+    @CreationTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dateReview;
 
     @ManyToOne
     @JoinColumn(name = "book_id")
     private Book book;
-    
+
     @ManyToOne
+    @JsonIgnore
     @JoinColumn(name = "user_detail_id")
     private UserDetail userDetail;
-
 }
