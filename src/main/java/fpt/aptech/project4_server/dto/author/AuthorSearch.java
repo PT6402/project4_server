f23199f3b2c53821ce2e60d3b89f6e2140f9a0c9
HieutranEproject <tranthienhieu146@gmail.com>
2024-07-15 01:08:30 +0700

    search

---
+++ src/main/java/fpt/aptech/project4_server/dto/author/AuthorSearch.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fpt.aptech.project4_server.dto.author;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author macos
+ */
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorSearch {
+    private int id;
+     private byte[] fileImage;
+    private String name;
+}
--- src/main/java/fpt/aptech/project4_server/repository/AuthorRepository.java
+++ src/main/java/fpt/aptech/project4_server/repository/AuthorRepository.java
@@ -1,8 +1,12 @@
 package fpt.aptech.project4_server.repository;
 
 import fpt.aptech.project4_server.entities.book.Author;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
-    
+      @Query("SELECT a FROM Author a WHERE a.name LIKE %:wordSearch%")
+    List<Author> searchByNameAuthor(@Param("wordSearch") String wordSearch);
 }
