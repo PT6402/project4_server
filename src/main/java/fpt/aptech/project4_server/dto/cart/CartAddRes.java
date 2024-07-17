a980637ccb30a86bab56c5f351f821d49e66ce73
HieutranEproject <tranthienhieu146@gmail.com>
2024-07-17 13:15:40 +0700

    chua co view

---
+++ src/main/java/fpt/aptech/project4_server/dto/cart/CartAddRes.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fpt.aptech.project4_server.dto.cart;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author macos
+ */
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartAddRes {
+    private int bookId;
+    private int packId;
+    private Boolean ibuy;
+}
